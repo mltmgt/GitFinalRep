54bb15e3fe35439c83348b6ae866c9b526faf944
mltmgt <meltem.zumba@gmail.com>
2022-09-29 03:48:25 +0300

    2nd commit

---
+++ src/FinalProject/FinalProjectg1.java
@@ -0,0 +1,14 @@
+package FinalProject;
+
+public class FinalProjectg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is the code for the merco");
+
+
+
+	}
+	
+
+}
