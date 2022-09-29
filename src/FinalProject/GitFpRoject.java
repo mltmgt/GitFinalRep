73a140aaf1b9e13d1ea93327a791a9812dbcec0a
mltmgt <meltem.zumba@gmail.com>
2022-09-29 03:11:10 +0300

    first commit

---
+++ src/FinalProject/GitFpRoject.java
@@ -0,0 +1,12 @@
+package FinalProject;
+
+public class GitFpRoject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this code for GitHube");
+
+	}
+
+}
